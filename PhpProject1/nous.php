0874f532e746cdaf574ff7a2ced574af62725455
osmltir <ldelevallez@gmail.com>
2013-04-17 23:42:17 +0200

    Envoi des fichiers

---
+++ PhpProject1/nous.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css"/>
+        <title>OSML Tir</title>
+		<link rel="icon" href="img/favicon.ico" type="image/ico" />
+	</head>
+
+    <body>
+		<header>
+            <?php include("entete.php"); ?>
+            <?php include("menu.php"); ?>
+        </header>
+    </body>
+</html>
