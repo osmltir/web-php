403c3d6bdef2af2cf1bf2d468426f77dd48efc76
osmltir <ldelevallez@gmail.com>
2013-04-17 23:45:48 +0200

    Envoi des fichiers

---
+++ PhpProject1/entete.php
@@ -0,0 +1,5 @@
+		<div id="entete">
+			<div class="element_entete">
+				<a href="index.php"> <img src="img/Header-osml.gif" alt="bannière"/>
+			</div>
+		</div>
---
+++ PhpProject1/menu.php
@@ -0,0 +1,13 @@
+		<div id="menu">
+			<div class="element_menu">
+				<div style="text-align : center;">
+                <ul id="menubarre">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="adherent.php">Adhérent</a></li>
+                    <li><a href="nous.php">Qui sommes nous ?</a></li>
+                    <li><a href="liens.php">Liens</a></li>
+                </ul>
+				</div>
+			</div>
+		</div>
